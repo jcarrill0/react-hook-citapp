fe3cdd9a538e9b2b7d0a94eeb9ccfef273252bfd
unknown <jcarrillo@SJ-STA-001-L349.clarocr.americamovil.ca1>
2020-01-05 15:05:39 -0600

    Primer commit proyecto citas react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,178 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, createFactory } from 'react';
+
+
+function Cita({cita, index, eliminarCita}) {
+
+  return(
+    <div className="cita">
+      <p>Mascota: <span>{cita.mascota}</span></p>
+      <p>Dueño: <span>{cita.propietario}</span></p>
+      <p>Fecha: <span>{cita.fecha}</span></p>
+      <p>Hora: <span>{cita.hora}</span></p>
+      <p>Síntomas: <span>{cita.sintomas}</span></p>
+      <button
+        onClick={() => eliminarCita(index)} 
+        type="button" className="button eliminar u-full-width">Eliminar X</button>
+    </div>
+  )
+}
+
+
+function Formulario({crearCita}) {
+
+  const stateInicial = {
+    mascota: '',
+    propietario: '',
+    fecha: '',
+    hora: '',
+    sintomas: '',
+  }
+
+  // Cita = state actual
+  // actualizarCita = fn para cambiar el state
+  const [cita, actualizarCita] = useState(stateInicial);
+
+  // Actualiza el state
+  const actualizarState = e => {
+    actualizarCita({
+      ...cita,
+      [e.target.name] : e.target.value
+    });
+  }
+
+  // Pasamos la cita al componente principal
+  const enviarCita = e => {
+    e.preventDefault();
+
+    // Pasar la cita hacia el componente principal 
+    crearCita(cita);
+
+    // Reiniciar el STATE (reiniciar el form)
+    actualizarCita(stateInicial);
+  }
+
+  return (
+    <>
+      <h2>Crear Cita</h2>
+
+      <form onSubmit={enviarCita}>
+          <label>Nombre Mascota</label>
+          <input 
+            type="text" 
+            name="mascota"
+            className="u-full-width" 
+            placeholder="Nombre Mascota" 
+            onChange={actualizarState}
+            value={cita.mascota}
+          />
+
+          <label>Nombre Dueño</label>
+          <input 
+            type="text" 
+            name="propietario"
+            className="u-full-width"  
+            placeholder="Nombre Dueño de la Mascota" 
+            onChange={actualizarState}
+            value={cita.propietario}
+          />
+
+          <label>Fecha</label>
+          <input 
+            type="date" 
+            className="u-full-width"
+            name="fecha"
+            onChange={actualizarState}
+            value={cita.fecha}
+          />               
+
+          <label>Hora</label>
+          <input 
+            type="time" 
+            className="u-full-width"
+            name="hora" 
+            onChange={actualizarState}
+            value={cita.hora}
+          />
+
+          <label>Sintomas</label>
+          <textarea 
+            className="u-full-width"
+            name="sintomas"
+            onChange={actualizarState}
+            value={cita.sintomas}
+          ></textarea>
+
+          <button type="submit" className="button-primary u-full-width">Agregar</button>
+      </form>
+    </>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  // Cargar las citas de local storage como el state inicial
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+
+  if(!citasIniciales) {
+    citasIniciales = [];
+  } 
+
+  const [citas, guardarCita] = useState(citasIniciales);
+
+  // Agregar nuevas citas al state 
+  const crearCita = cita => {
+    // Tomar una copia del state y agregar nuevo cliente 
+    const nuevasCitas = [...citas, cita];
+    // Almacenamos en el state
+    guardarCita(nuevasCitas);
+  }
+
+  // Elimina las citas del state 
+  const eliminarCita = (index) => {
+    const nuevasCitas = [...citas];
+
+    nuevasCitas.splice(index, 1);
+    guardarCita(nuevasCitas);
+  }
+
+  useEffect(() => {
+    let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+
+    if(citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas));
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas] )
+
+
+  // Cargar condicionalmente un titulo
+  const titulo = Object.keys(citas).length === 0 ? 'No hay Citas' : 'Administrar las Citas aqui'
+
+  return ( 
+    <>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+          <div className="row">
+            <div className="one-half column">
+                <Formulario 
+                  crearCita={crearCita}
+                />
+            </div>
+            <div className="one-half column">
+              <h2>{titulo}</h2>
+              {citas.map((cita, index) => (
+                  <Cita 
+                    key={index}
+                    index={index}
+                    cita={cita}
+                    eliminarCita={eliminarCita}
+                  />
+              ))}
+            </div>
+          </div>
+      </div>
+    </>
   );
 }
 
